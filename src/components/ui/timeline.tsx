35f8f7495bf23650ba40b61cacaf6bbfc87dbc0b
Facundo Pettersson <visualstudio.creator23@gmail.com>
2024-11-08 16:06:47 -0300

    feat: Changes

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -35,7 +35,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       ref={containerRef}
     >
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg font-bold md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
+        <h2 className="text-lg font-bold md:text-4xl mb-4 text-white dark:text-white max-w-4xl">
           Experiencia
         </h2>
         <p className="text-white dark:text-white text-sm md:text-base max-w-2xl">
